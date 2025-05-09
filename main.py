099b598bbf9690b00fe74f5dd6337b0508f0e1e7
Benjos <benjos@MacBook-Pro-de-Benjamin.local>
2025-05-09 21:57:08 +0200

    Petite modif

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from app.ui import afficher_interface
 
+import spacy
+import subprocess
+
+# Télécharger le modèle français si pas installé
+try:
+    spacy.load("fr_core_news_sm")
+except OSError:
+    subprocess.run(["python", "-m", "spacy", "download", "fr_core_news_sm"])
+
+
 if __name__ == "__main__":
     afficher_interface()  # Lance l’interface Streamlit
